a9bf77a491fcd882752d30c1bfb35116f3f3fd0b
cholazzzb <nicsphehehe@gmail.com>
2021-03-09 21:45:44 +0700

    Add Apollo Client to connect to graphQL SpaceX Land

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,46 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
-export default function Home() {
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: "https://api.spacex.land/graphql/",
+    cache: new InMemoryCache(),
+  });
+
+  const { data } = await client.query({
+    query: gql`
+      query GetLaunches {
+        launchesPast(limit: 10) {
+          id
+          mission_name
+          launch_date_local
+          launch_site {
+            site_name_long
+          }
+          links {
+            article_link
+            video_link
+            mission_patch
+          }
+          rocket {
+            rocket_name
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      launches: data.launchesPast,
+    },
+  };
+}
+
+export default function Home({ launches }) {
+  console.log("launches", launches);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,38 +54,28 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {launches.map((launch) => {
+            return (
+              <a
+                key={launch.id}
+                href={launch.links.video_link}
+                className={styles.card}
+              >
+                <h3>{launch.mission_name}</h3>
+                <p>
+                  <strong>Launch Date:</strong>{" "}
+                  {new Date(launch.launch_date_local).toLocaleDateString(
+                    "en-US"
+                  )}
+                </p>
+              </a>
+            );
+          })}
         </div>
       </main>
 
@@ -56,10 +85,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
